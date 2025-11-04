856bb403edde859ba2451d2901df7fd5427fd764
a3 <a3@labsis.frc.utn.edu.ar>
2025-11-04 14:05:51 -0300

    Agregado java

---
+++ dsi_ppai/src/main/java/com/example/dsi_ppai/DsiPpaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsi_ppai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsiPpaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsiPpaiApplication.class, args);
+	}
+
+}
---
+++ dsi_ppai/src/test/java/com/example/dsi_ppai/DsiPpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsi_ppai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsiPpaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
